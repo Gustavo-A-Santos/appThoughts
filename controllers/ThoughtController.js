a9e146137e0af2649fdc590bb6023ab4507714fe
Gustavo <gustavosantos4991@gmail.com>
2023-08-15 22:24:02 -0300

    Aula 1508 - Finalização Projeto Thoughts

--- controllers/ThoughtController.js
+++ controllers/ThoughtController.js
@@ -62,4 +62,65 @@ module.exports = class ThoughtController{
             res.render('thoughts/home', {thoughts, thoughtsQty, search})
         }).catch((err) => console.error(err))
     }
+
+    static createThought(req, res){
+        res.render('thoughts/create')
+    }
+
+    static createThoughtSave(req, res){
+        const thought = {
+            title: req.body.title,
+            UserId: req.session.userid
+        }
+
+        Thought.create(thought)
+        .then(() => {
+            req.flash('message','Pensamento foi criado com sucesso.')
+            req.session.save(() =>{
+                res.redirect('/thoughts/dashboard')
+            })
+        })
+        .catch((err) => console.log(err))
+    }
+
+    static removeThought(req, res){
+        const id = req.body.id
+
+        Thought.destroy({where: {id:id}})
+        .then(() => {
+            req.flash('message','Pensamento Excluido com Sucesso.')
+            req.session.save(() =>{
+                res.redirect('/thoughts/dashboard')
+            })
+        })
+        .catch((err) => console.log(err))
+    }
+
+    static updateThought(req, res){
+        const id = req.params.id
+
+        Thought.findOne({ where: {id:id}, raw: true})
+        .then((thought) => {
+            res.render('thoughts/edit',{thought})
+        })
+        .catch((err) => console.log(err))
+    }
+
+    static updateThoughtPost(req, res){
+        const id = req.body.id
+
+        const thought = {
+            title: req.body.title,
+            description: req.body.description
+        }
+
+        Thought.update(thought, {where: {id:id}})
+        .then((thought) => {
+            req.flash('message', 'Pensamento alterado com sucesso!')
+            req.session.save(() => {
+                res.render('thought/dashboard')
+            })
+        })
+        .catch((err) => console.error(err))
+    }
 }
